1707c5f00912a84293b579fab3e48ce43492dc25
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-03 22:25:43 +0200

    Add files via upload

---
+++ Task02_Knights_of_Honor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Task02_Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Action<string> printNamesWithTitle = name => Console.WriteLine($"Sir {name}");
+
+            foreach (string item in names)
+            {
+                printNamesWithTitle(item);
+            }
+            
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
