e5d015279bc7dfd6ba7f5b94f86484a8afd2c4dd
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-03 22:36:22 +0200

    Add files via upload

---
+++ Task07_Predicate_For_Names/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Task07_Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthLimit = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Func<string, bool> filter = n => n.Length <= lengthLimit;
+
+            foreach (string name in names)
+            {
+                if(filter(name))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
