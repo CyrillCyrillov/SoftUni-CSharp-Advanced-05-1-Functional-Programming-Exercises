09ed49689538590f5d75588da617fbaebefb6bd5
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-03 22:35:36 +0200

    Add files via upload

---
+++ Task06_Reverse_and_Exclude_A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task06_Reverse_and_Exclude_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int divisibleNumber = int.Parse(Console.ReadLine());
+
+            Func<int, bool> notDivisible = n => n % divisibleNumber != 0;
+
+            numbers = (List<int>)numbers.Where(notDivisible).Reverse().ToList();
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
