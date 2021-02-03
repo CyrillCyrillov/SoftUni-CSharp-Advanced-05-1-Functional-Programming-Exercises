6c53f79b1198b05aba0c70563b625875942a97e9
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-03 22:29:35 +0200

    Add files via upload

---
+++ Task03_Custom_Min_Function/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Task03_Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Func<int[], int> findMin = n => n.Min();
+            
+            Console.WriteLine(findMin(numbers));
+        }
+    }
+}
