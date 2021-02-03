a252a25d788192f5fd91b4eee3b49acdbd1916ab
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-03 22:33:00 +0200

    Add files via upload

---
+++ Task05_Applied_Arithmetics/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace Task05_Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Action<string> Manipulations = (type) =>
+            {
+                if (type.ToUpper() == "ADD")
+                {
+                    numbers = numbers.Select(n => n + 1).ToArray();
+                }
+
+                if (type.ToUpper() == "MULTIPLY")
+                {
+                    numbers = numbers.Select(n => n * 2).ToArray();
+                }
+
+                if (type.ToUpper() == "SUBTRACT")
+                {
+                    numbers = numbers.Select(n => n - 1).ToArray();
+                }
+
+                if (type.ToUpper() == "PRINT")
+                {
+                    Console.WriteLine(string.Join(' ', numbers));
+                }
+
+            };
+
+            while (true)
+            {
+                string comand = Console.ReadLine();
+
+                if (comand.ToUpper() == "END")
+                {
+                    break;
+                }
+
+                Manipulations(comand);
+
+            }
+
+        }   
+            
+    }
+}
