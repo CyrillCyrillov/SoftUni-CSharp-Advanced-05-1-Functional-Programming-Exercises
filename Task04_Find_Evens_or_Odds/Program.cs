a6c3adbe3eda4a857c7950bb4c6a38e8935703a4
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-03 22:31:03 +0200

    Add files via upload

---
+++ Task04_Find_Evens_or_Odds/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Task04_Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] limits = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();            
+            
+            int minLimit = limits[0];
+            int maxLimit = limits[1];
+            string typeNumber = Console.ReadLine();
+
+            Predicate<int> evenNumber = x => { return x % 2 == 0; };
+            Predicate<int> oddNumber = x => { return x % 2 != 0; };
+
+            for (int i = minLimit; i <= maxLimit; i++)
+            {
+                if(typeNumber == "odd")
+                {
+                    if(oddNumber(i))
+                    {
+                        Console.Write($"{i} ");
+                    }
+                }
+                if(typeNumber == "even")
+                {
+                    if(evenNumber(i))
+                    {
+                        Console.Write($"{i} ");
+                    }
+                }
+            }
+            
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
