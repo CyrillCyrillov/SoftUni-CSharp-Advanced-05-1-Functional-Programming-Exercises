161c262f7dd06b4e050ec4cab69842b513bf6c30
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-04 21:18:37 +0200

    Add files via upload

---
+++ Task09_List_of_Predicates_A/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Task09_List_of_Predicates_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxLimit = int.Parse(Console.ReadLine());
+
+            int[] divisibles = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+
+
+            for (int i = 1; i <= maxLimit; i++)
+            {
+                bool isValid = true;
+                Predicate<int> isNodivible = x => i % x != 0;
+
+                foreach (int elelment in divisibles)
+                {
+                    if(isNodivible(elelment))
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+
+                if(isValid)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
