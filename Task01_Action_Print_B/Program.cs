468e3e87f69cf69be0c9d9298fb481beedff0a80
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-04 23:22:19 +0200

    Add files via upload

---
+++ Task01_Action_Print_B/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Task01_Action_Print_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Action<string> printName = curentName => Console.WriteLine(curentName);
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                printName(names[i]);
+            }
+
+            
+        }
+    }
+}
