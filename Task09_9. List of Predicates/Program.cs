12c5a8a7cf1cb696a49142bb41bc74686302f8a9
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-04 21:17:33 +0200

    Add files via upload

---
+++ Task09_9. List of Predicates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Task09_List_of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxLimit = int.Parse(Console.ReadLine());
+
+            int[] divisibles = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            
+            
+            for (int i = 1; i <= maxLimit; i++)
+            {
+                if(divisibles.All(n => i % n == 0))
+                {
+                    Console.Write($"{(i)} ");
+                }
+            }
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
