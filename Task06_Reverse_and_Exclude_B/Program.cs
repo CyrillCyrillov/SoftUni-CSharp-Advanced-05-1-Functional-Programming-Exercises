5e7084e8e894968f3ddf14af20f7c788c13b1d04
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-04 21:15:02 +0200

    Add files via upload

---
+++ Task06_Reverse_and_Exclude_B/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Task06_Reverse_and_Exclude_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int divisibleNumber = int.Parse(Console.ReadLine());
+
+            Func<int, bool> yesPrint = (n => n % divisibleNumber != 0);
+
+
+            for (int i = numbers.Length - 1; i>= 0; i--)
+			{
+                if(yesPrint(numbers[i]))
+                {
+                    Console.Write($"{numbers[i]} ");
+                }
+			}
+            
+        }
+    }
+}
