f234612da851c668df859ad61f6d72cd7ffc47c0
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-04 22:22:01 +0200

    Add files via upload

---
+++ Task12_TriFunction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Task12_TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int charactersSum = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split().ToArray();
+
+            Func<string, int, bool> isValidWord = (str, num) => str.ToCharArray().Select(ch => (int)ch).Sum() >= num;
+
+            Func<string[], int, Func<string, int, bool>, string> firstValidName = (arr, num, func) => arr.FirstOrDefault(str => func(str, num));
+
+            string result = firstValidName(names, charactersSum, isValidWord);
+            
+            
+            Console.WriteLine(result);
+        }
+    }
+}
